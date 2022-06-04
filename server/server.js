7fd44d82d3b5508e3eb83522eaf0f174f381b84d
Shevait Verma <shaveitverma333@gmail.com>
2022-06-04 21:03:26 +0530

    implemented frontend

--- server/server.js
+++ server/server.js
@@ -6,12 +6,14 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URI = "mongodb+srv://rexxy:962239@auth1.aeccu.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
